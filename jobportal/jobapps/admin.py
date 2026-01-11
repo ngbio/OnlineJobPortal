bd8215230d4848b4ed0e079f57738aea7e5f7d8b
Nguyễn Thuận <thuantokutou@gmail.com>
2026-01-11 20:34:06 +0700

    add stats for admin

--- jobportal/jobapps/admin.py
+++ jobportal/jobapps/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.utils.safestring import mark_safe
 from jobapps.models import JobPost, Applications, User, Comment, Category
+from django.urls import path
+from django.db.models import Count
+from django.template.response import TemplateResponse
 
 
 class ApplicationsAdmin(admin.ModelAdmin):
@@ -18,6 +21,16 @@ class UserAdmin(admin.ModelAdmin):
 
 class MyAdminSite(admin.AdminSite):
     site_header = 'Job Post'
+
+    def get_urls(self):
+        return [path('stats-view/', self.stats_view)] + super().get_urls()
+
+    def stats_view(self, request):
+        stats_category = Category.objects.annotate(count=Count('jobpost')).values('id', 'name', 'count')
+        stats_application = JobPost.objects.annotate(count=Count('applications')).values('id', 'name', 'count')
+
+        return TemplateResponse(request, 'admin/stats.html', {'stats_application': stats_application, 'stats_category': stats_category})
+
 
 admin_site = MyAdminSite(name='Job Post')
 
